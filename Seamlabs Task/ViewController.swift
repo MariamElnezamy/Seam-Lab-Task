7c67d831519489e0a524f2ad8a56802adbacb48b
Mariam Hassan <Mariam.Hassan@mdi.com.eg>
2023-09-04 16:24:09 +0300

    Initial Commit

---
+++ Seamlabs Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seamlabs Task
+//
+//  Created by Mariam on 04/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
